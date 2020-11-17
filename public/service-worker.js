db97b5762945a04e4757043cadee05e0ec8c2c68
carl-33 <koepkecj@gmail.com>
2020-11-16 20:28:07 -0600

    debugging service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 // declaring our static and data caches
-const CACE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v2";
 
 // everything in the public folder to be added to the cache
-const staticFuilestoPreCache = [
+const staticFilestoPreCache = [
     "/",
     "db.js",
     "styles.css",
@@ -16,9 +16,9 @@ const staticFuilestoPreCache = [
 // installing the service-worker
 self.addEventListener('install', function(evt){
     evt.waitUntil(
-        caches.open(CACE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log("precached those files for ya");
-            return cache.addAll(staticFuilestoPreCache);
+            return cache.addAll(staticFilestoPreCache);
         })
     );
     self.skipWaiting();
@@ -29,9 +29,9 @@ self.addEventListener("activate", function(evt){
     evt.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
-                keylist.map(key => {
-                    if(key !== CACE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log(`Removing ${key} from cahce`);
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log(`Removing ${key} from cache`);
                         return caches.delete(key);
                     }
                 })
@@ -45,22 +45,22 @@ self.addEventListener("fetch", function(evt) {
     const {url} = evt.request;
     if(url.includes("/api/transaction")){
         evt.respondWith(
-            cavhes.open(DATA_CACHE_NAME).then(cahce =>{
+            caches.open(DATA_CACHE_NAME).then(cache =>{
                 return fetch(evt.request)
                 .then(response => {
                     if(response.status === 200) {
-                        cache.put(evt.request, respponse.clone());
+                        cache.put(evt.request, response.clone());
                     }
                     return response;
                 })
                 .catch(err => {
-                    return cache.match(evt.requesst);
+                    return cache.match(evt.request);
                 });
             }).catch(err => console.log(err))
         );
     } else {
         evt.respondWith(
-            caches.open(CACE_NAME).then(cache => {
+            caches.open(CACHE_NAME).then(cache => {
                 return cache.match(evt.request).then(response => {
                     return response || fetch(evt.request);
                 });
